9837f510f963f473fdd8aa86fed3149072e30d95
stanislav <stasrttm@gmail.com>
2023-11-06 11:55:43 +0300

    test

--- TCAshka/TCAshka/ApplicationLayer/TCAshkaApp.swift
+++ TCAshka/TCAshka/ApplicationLayer/TCAshkaApp.swift
@@ -23,14 +23,6 @@ struct TCAshkaApp: App {
                     reducer: MainReducer()
                 )
             )
-        }
-        
-        
-        
-        
-        
-        
-        
-        
+        } 
     }
 }
